6629f44cea64cef9ae61203ce1e14e3d771081b3
Semih <54319361+semihV23@users.noreply.github.com>
2021-04-13 20:49:23 +0300

    Add files via upload

---
+++ vds.ps1
@@ -0,0 +1,7 @@
+wget https://dl.google.com/tag/s/*/update2/installers/ChromeSetup.exe -O chromeSetup.exe
+wget https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-601tr.exe -O winrarSetup.exe
+wget https://www.7-zip.org/a/7z1900-x64.exe -O 7zipSetup.exe
+
+#start chromeSetup.exe
+#start winrarSetup.exe
+#start 7zipSetup.exe
