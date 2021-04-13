b950889e2ddf4fe778229671998086c76b906785
Semih <54319361+semihV23@users.noreply.github.com>
2021-04-13 20:59:07 +0300

    Update vds.ps1

--- vds.ps1
+++ vds.ps1
@@ -1,3 +1,5 @@
+mkdir vds/
+
 wget https://dl.google.com/tag/s/*/update2/installers/ChromeSetup.exe -O chromeSetup.exe
 wget https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-601tr.exe -O winrarSetup.exe
 wget https://www.7-zip.org/a/7z1900-x64.exe -O 7zipSetup.exe
