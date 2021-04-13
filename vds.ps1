882659e4022867ae6f7d8ea7d5ae01aeaaa05364
Semih <54319361+semihV23@users.noreply.github.com>
2021-04-13 21:01:52 +0300

    Hatalar çözüldü

--- vds.ps1
+++ vds.ps1
@@ -1,5 +1,8 @@
 mkdir vds/
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
+[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
+
 wget https://dl.google.com/tag/s/*/update2/installers/ChromeSetup.exe -O vds/chromeSetup.exe
 wget https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-601tr.exe -O vds/winrarSetup.exe
 wget https://www.7-zip.org/a/7z1900-x64.exe -O vds/7zipSetup.exe
